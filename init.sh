fdc5bcbc1c26bba205e82df249e0b753f3b4f049
gabrielcfvg <gabrielcfvg@gmail.com>
2023-09-10 16:32:14 -0300

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+git clone https://github.com/Microsoft/vcpkg.git
+./vcpkg/bootstrap-vcpkg.sh --disableMetrics
+
---
+++ src/kbvm/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+
+
+int main() {
+    
+    std::cout << "KriegBoar VM" << std::endl;
+}
